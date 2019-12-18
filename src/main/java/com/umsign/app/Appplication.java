5062a574225f2e96432f8e75144d7b8911f1b6a8
ableKiHo <able.kiho.um@gmail.com>
2019-12-18 14:52:04 +0900

    distinguish the method - step 2

--- src/main/java/com/umsign/app/Appplication.java
+++ src/main/java/com/umsign/app/Appplication.java
@@ -11,13 +11,18 @@ public class Appplication {
         int serverPort = 8000;
         HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
         server.createContext("/api/hello", httpExchange -> {
-            String respText = "Hello!";
-            httpExchange.sendResponseHeaders(200, respText.getBytes().length); //response code and length
-            OutputStream outputStream = httpExchange.getResponseBody();
-            outputStream.write(respText.getBytes());
-            outputStream.flush();  // 출력 스트림과 버퍼된 출력 바이트를 강제로 쓰게 한다.
-            // 기본적인 출력 스트림은 버퍼에 데이터가 가득 차면 그때 데이터를 출려기키는데 이 메소드를 사용하면 저장된 데이터의 크기에 관계없이 바로 출력
-            httpExchange.close();
+            if("GET".equals(httpExchange.getRequestMethod())) {
+                String respText = "Hello!";
+                httpExchange.sendResponseHeaders(200, respText.getBytes().length); //response code and length
+                OutputStream outputStream = httpExchange.getResponseBody();
+                outputStream.write(respText.getBytes());
+                outputStream.flush();  // 출력 스트림과 버퍼된 출력 바이트를 강제로 쓰게 한다.
+                // 기본적인 출력 스트림은 버퍼에 데이터가 가득 차면 그때 데이터를 출려기키는데 이 메소드를 사용하면 저장된 데이터의 크기에 관계없이 바로 출력
+                httpExchange.close();
+            }else {
+                httpExchange.sendResponseHeaders(405, -1); // 405 Method Not Allowed
+            }
+
         });
         server.setExecutor(null);
         server.start();
